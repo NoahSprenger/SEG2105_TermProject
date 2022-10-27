1f27c517bd242cbc1d18fd98154c25b5aa060fd5
Samuel Krutis <samuel.krutis@gmail.com>
2022-10-27 12:14:38 -0400

    Created User and All Subclasses

---
+++ app/src/main/java/SEGTermProject/G15/Admin.java
@@ -0,0 +1,22 @@
+package SEGTermProject.G15;
+
+public class Admin extends User{
+    String userName = "admin";
+    String password = "admin123";
+    String userID = "0";
+    private void addCourse(){
+
+    }
+
+    private void deleteCourse(){
+
+    }
+
+    private void editCourse(){
+
+    }
+
+    private void deleteUser(){
+
+    }
+}
