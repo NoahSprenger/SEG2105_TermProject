d0719dbf2401d634cc9b2b4463d0e173d8437c6c
Marwan Mashaly <mmash102@uottawa.ca>
2022-10-31 23:14:48 -0400

    fix Admin page UI

--- app/src/main/java/SEGTermProject/G15/AdminActivity.java
+++ app/src/main/java/SEGTermProject/G15/AdminActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
-    Button btnUserUtilities, btnCourseUtilities;
+    private Button btnUserUtilities, btnCourseUtilities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
