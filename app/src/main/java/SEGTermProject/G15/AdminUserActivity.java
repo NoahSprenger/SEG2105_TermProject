d4ded57edfcba6d279f3a4916478e92a76fe7ad0
Noah Sprenger <noahsprengersonline@gmail.com>
2022-10-31 22:29:08 -0400

    add input for admin user activity

--- app/src/main/java/SEGTermProject/G15/AdminUserActivity.java
+++ app/src/main/java/SEGTermProject/G15/AdminUserActivity.java
@@ -4,22 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminUserActivity extends AppCompatActivity {
     private Button btnDeleteStudent, btnDeleteInstructor;
-
+    private EditText InputInstructorName, InputStudentName;
+    private String InstructorName, StudentName;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DBHandler db = new DBHandler();
         setContentView(R.layout.activity_admin_user);
         btnDeleteInstructor = findViewById(R.id.btnAdminDeleteInstructor);
         btnDeleteStudent = findViewById(R.id.btnAdminDeleteStudent);
+        InputInstructorName = findViewById(R.id.adminInstructorName);
+        InputStudentName = findViewById(R.id.adminStudentName);
         btnDeleteStudent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                StudentName = InputStudentName.getText().toString();
+                db.deleteUser(StudentName);
                 Intent intent = new Intent(AdminUserActivity.this, AdminActivity.class);
                 startActivity(intent);
                 finish();
@@ -29,6 +34,8 @@ public class AdminUserActivity extends AppCompatActivity {
         btnDeleteInstructor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                InstructorName = InputInstructorName.getText().toString();
+                db.deleteUser(InstructorName);
                 Intent intent = new Intent(AdminUserActivity.this, AdminActivity.class);
                 startActivity(intent);
                 finish();
